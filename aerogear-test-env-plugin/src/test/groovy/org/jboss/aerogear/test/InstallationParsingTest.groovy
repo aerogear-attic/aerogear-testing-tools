c4df2ce730c57ebe18589c4cb77518e390992d68
Karel Piwko <kpiwko@redhat.com>
2014-07-03 19:40:25 +0200

    Unignored installation test

--- aerogear-test-env-plugin/src/test/groovy/org/jboss/aerogear/test/InstallationParsingTest.groovy
+++ aerogear-test-env-plugin/src/test/groovy/org/jboss/aerogear/test/InstallationParsingTest.groovy
@@ -1,23 +1,17 @@
 package org.jboss.aerogear.test
 
-import org.arquillian.spacelift.execution.Tasks
-import org.arquillian.spacelift.process.ProcessResult
-import org.gradle.api.Project
-import org.gradle.testfixtures.ProjectBuilder
-import org.jboss.aerogear.test.openshift.CreateOpenshiftCartridge;
-import org.jboss.aerogear.test.utils.KillJavas
-import org.junit.Ignore;
-import org.junit.Test
-
 import static org.hamcrest.CoreMatchers.*
 import static org.junit.Assert.assertThat
+
+import org.gradle.api.Project
+import org.gradle.testfixtures.ProjectBuilder
+import org.junit.Test
 
 /**
  * Asserts that installations can be specified without home
  * @author <a href="mailto:kpiwko@redhat.com">Karel Piwko</a>
  *
  */
-@Ignore
 public class InstallationParsingTest {
 
     @Test
