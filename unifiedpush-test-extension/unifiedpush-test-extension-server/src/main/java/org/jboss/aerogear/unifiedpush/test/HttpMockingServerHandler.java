2f58b04b0e85923358cd1b039422d51e0193cf34
Adam Saleh <asaleh@redhat.com>
2014-12-11 15:16:10 +0100

    Added netty server to mock gcm and log data to be displayed by SenderStatisticsEndpoint

---
+++ unifiedpush-test-extension/unifiedpush-test-extension-server/src/main/java/org/jboss/aerogear/unifiedpush/test/HttpMockingServerHandler.java
@@ -0,0 +1,138 @@
+package org.jboss.aerogear.unifiedpush.test;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.android.gcm.server.Message;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.*;
+import io.netty.util.CharsetUtil;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import static io.netty.handler.codec.http.HttpHeaders.Names.*;
+import static io.netty.handler.codec.http.HttpResponseStatus.*;
+import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
+
+/**
+ * Created by asaleh on 12/11/14.
+ */
+public class HttpMockingServerHandler extends SimpleChannelInboundHandler<Object> {
+
+    private HttpRequest request;
+    /** Buffer that stores the response content */
+    private final StringBuilder buf = new StringBuilder();
+    private final StringBuilder contentBuf = new StringBuilder();
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ctx.flush();
+    }
+
+    String uri = null;
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, Object msg) {
+        if (msg instanceof HttpRequest) {
+            HttpRequest request = this.request = (HttpRequest) msg;
+            uri = request.getUri();
+            if (HttpHeaders.is100ContinueExpected(request)) {
+                send100Continue(ctx);
+            }
+
+
+
+
+        }
+
+        if (msg instanceof HttpContent) {
+            HttpContent cnt = (HttpContent) msg;
+            ByteBuf content =cnt.content();
+
+            contentBuf.append(content.toString(CharsetUtil.UTF_8));
+            if (msg instanceof LastHttpContent) {
+
+                buf.setLength(0);
+                if(uri.contains("gcm")) {
+                    ObjectMapper mapper = new ObjectMapper();
+                    try {
+                        GCMessage o = mapper.readValue(contentBuf.toString(), GCMessage.class);
+                        SenderStatistics stats = new SenderStatistics();
+                        Message.Builder gcmMessage = new Message.Builder();
+
+                        gcmMessage.setData(o.data);
+                        if(o.collapse_key!=null) {
+                            gcmMessage.collapseKey(o.collapse_key);
+                        }
+                        if(o.delay_while_idle!=null){
+                            gcmMessage.delayWhileIdle(o.delay_while_idle.booleanValue());
+                        }
+                        if(o.time_to_live!=null){
+                            gcmMessage.timeToLive(o.time_to_live.intValue());
+                        }
+
+                        stats.gcmMessage = gcmMessage.build();
+                        stats.deviceTokens = o.registration_ids;
+
+                        SenderStatisticsEndpoint.setSenderStatistics(stats);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+
+                    HashMap<String, Object> jsonResponse = new HashMap<String, Object>();
+                    jsonResponse.put("failure", new Integer(0));
+                    jsonResponse.put("canonical_ids", new Integer(0));
+                    try {
+                        buf.append(mapper.writeValueAsString(jsonResponse));
+                    } catch (JsonProcessingException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+
+                writeResponse((HttpObject)msg,ctx);
+            }
+        }
+
+    }
+
+
+    private boolean writeResponse(HttpObject currentObj, ChannelHandlerContext ctx) {
+        // Decide whether to close the connection or not.
+        boolean keepAlive = HttpHeaders.isKeepAlive(request);
+        // Build the response object.
+        FullHttpResponse response = new DefaultFullHttpResponse(
+                HTTP_1_1, currentObj.getDecoderResult().isSuccess()? OK : BAD_REQUEST,
+                Unpooled.copiedBuffer(buf.toString(), CharsetUtil.UTF_8));
+
+        response.headers().set(CONTENT_TYPE, "application/json");
+
+        if (keepAlive) {
+            // Add 'Content-Length' header only for a keep-alive connection.
+            response.headers().set(CONTENT_LENGTH, response.content().readableBytes());
+            // Add keep alive header as per:
+            // - http://www.w3.org/Protocols/HTTP/1.1/draft-ietf-http-v11-spec-01.html#Connection
+            response.headers().set(CONNECTION, HttpHeaders.Values.KEEP_ALIVE);
+        }
+
+        // Write the response.
+        ctx.write(response);
+
+        return keepAlive;
+    }
+
+    private void send100Continue(ChannelHandlerContext ctx) {
+        FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1, CONTINUE);
+        ctx.write(response);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
---
+++ unifiedpush-test-extension/unifiedpush-test-extension-server/src/main/java/org/jboss/aerogear/unifiedpush/test/SenderStatisticsEndpoint.java
@@ -0,0 +1,40 @@
+package org.jboss.aerogear.unifiedpush.test;
+
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * Created by asaleh on 12/11/14.
+ */
+@Stateless
+@TransactionAttribute
+@Path("/senderStats")
+// FIXME move to src/main
+public class SenderStatisticsEndpoint {
+
+    private static SenderStatistics senderStatistics;
+    public static void setSenderStatistics(SenderStatistics statistics){
+        senderStatistics = statistics;
+    }
+
+    public static void clearSenderStatistics(){
+        senderStatistics=null;
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response listAllStatistics() {
+        return Response.ok(senderStatistics).build();
+    }
+    @DELETE
+    public Response resetStatistics() {
+        clearSenderStatistics();
+        return Response.noContent().build();
+    }
+}
