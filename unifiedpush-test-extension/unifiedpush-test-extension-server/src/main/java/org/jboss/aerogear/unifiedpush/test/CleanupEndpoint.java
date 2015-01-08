5cfa5224882dee8d90f47885e1cea96f85e0f78b
Tadeas Kriz <tadeas.kriz@gmail.com>
2015-01-08 10:58:47 +0100

    Fix checkstyle errors.

--- unifiedpush-test-extension/unifiedpush-test-extension-server/src/main/java/org/jboss/aerogear/unifiedpush/test/CleanupEndpoint.java
+++ unifiedpush-test-extension/unifiedpush-test-extension-server/src/main/java/org/jboss/aerogear/unifiedpush/test/CleanupEndpoint.java
@@ -27,8 +27,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.HashMap;
-import java.util.Map;
 
 @Stateless
 @Path("/cleanup")
