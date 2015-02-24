620b09b34a0b89be900ae3b6ea1d47140093c687
Tadeas Kriz <tadeas.kriz@gmail.com>
2015-02-24 12:17:57 +0100

    Change invalidation token prefix to be CAFEDEAD instead of cafedead-.

--- aerogear-ups-rest-api/src/main/java/org/jboss/aerogear/test/api/installation/Tokens.java
+++ aerogear-ups-rest-api/src/main/java/org/jboss/aerogear/test/api/installation/Tokens.java
@@ -2,5 +2,6 @@ package org.jboss.aerogear.test.api.installation;
 
 public class Tokens {
 
-    public static final String TOKEN_INVALIDATION_PREFIX = String.format("%08X-", 0xcafedead).toLowerCase();
+    public static final String TOKEN_INVALIDATION_PREFIX = String.format("%08X", 0xcafedead).toUpperCase();
+
 }
