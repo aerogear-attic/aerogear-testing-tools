4e6f922b6000e078d523193d3d1a34ba61960db5
Tadeas Kriz <tadeas.kriz@gmail.com>
2015-04-09 16:09:41 +0200

    Move ups-rest-api and unifiedpush-test-extension to integration-tests.

--- aerogear-ups-rest-api/src/main/java/org/jboss/aerogear/test/api/installation/Tokens.java
+++
@@ -1,7 +0,0 @@
-package org.jboss.aerogear.test.api.installation;
-
-public class Tokens {
-
-    public static final String TOKEN_INVALIDATION_PREFIX = String.format("%08X", 0xcafedead).toLowerCase();
-
-}
--- unifiedpush-test-extension/unifiedpush-test-extension-common/src/main/java/org/jboss/aerogear/unifiedpush/test/Tokens.java
+++
@@ -1,8 +0,0 @@
-package org.jboss.aerogear.unifiedpush.test;
-
-/**
- * Created by asaleh on 12/11/14.
- */
-public class Tokens {
-    public static final String TOKEN_INVALIDATION_PREFIX = String.format("%08X", 0xcafedead).toLowerCase();
-}
